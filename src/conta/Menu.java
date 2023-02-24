319b8dbb2e51a2108b08660966326ee297df6bce
Guilherme Amorim <guilhermedeamorim098@gmail.com>
2023-02-24 15:23:36 -0300

    TASK-05 – Construcao da Classe ContaController e Atualização do Menu – Parte I

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import conta.controller.ContaController;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
@@ -12,29 +13,30 @@ public class Menu {
 
 	public static void main(String[] args) {
 
-		// Testa classe Conta Corrente
+		ContaController contaController = new ContaController();
 
-		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "Vinicius Amorim", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-		System.out.println();
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
 
-		// Testa classe Conta Poupança
-		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Susy Tateshita", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
-		System.out.println();
+		System.out.println("\nCriar Contas\n");
+
+		ContaCorrente cc1 = new ContaCorrente(contaController.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contaController.cadastrar(cc1);
+
+		ContaCorrente cc2 = new ContaCorrente(contaController.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contaController.cadastrar(cc2);
+
+		ContaPoupanca cp1 = new ContaPoupanca(contaController.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contaController.cadastrar(cp1);
+
+		ContaPoupanca cp2 = new ContaPoupanca(contaController.gerarNumero(), 125, 2, "Juliana Ramos", 8000f, 15);
+		contaController.cadastrar(cp2);
+
+		contaController.listarTodas();
 
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-
-		int opcao;
 
 		while (true) {
 
@@ -72,41 +74,126 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n");
+				System.out.println("Digite o Numero da Agência: ");
+				agencia = scan.nextInt();
+				System.out.println("Digite o Nome do Titular: ");
+				scan.skip("\\R?");
+				titular = scan.nextLine();
+
+				do {
+					System.out.println("Digite o Tipo da Conta (1-CC ou 2-CP): ");
+					tipo = scan.nextInt();
+				} while (tipo < 1 && tipo > 2);
+
+				System.out.println("Digite o Saldo da Conta (R$): ");
+				saldo = scan.nextFloat();
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o Limite de Crédito (R$): ");
+					limite = scan.nextFloat();
+					contaController.cadastrar(
+							new ContaCorrente(contaController.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 -> {
+					System.out.println("Digite o dia do Aniversario da Conta: ");
+					aniversario = scan.nextInt();
+					contaController.cadastrar(new ContaPoupanca(contaController.gerarNumero(), agencia, tipo, titular,
+							saldo, aniversario));
+				}
+				}
+
 				keyPress();
 				break;
+
 			case 2:
 				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n");
+				contaController.listarTodas();
 				keyPress();
 				break;
+
 			case 3:
 				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n");
+				System.out.print("Digite o número da conta: ");
+				numero = scan.nextInt();
+				contaController.procurarPorNumero(numero);
+
 				keyPress();
 				break;
+
 			case 4:
 				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n");
+				System.out.println("Digite o número da conta: ");
+				numero = scan.nextInt();
+
+				if (contaController.buscarNaCollection(numero) != null) {
+
+					System.out.println("Digite o Numero da Agência: ");
+					agencia = scan.nextInt();
+					System.out.println("Digite o Nome do Titular: ");
+					scan.skip("\\R?");
+					titular = scan.nextLine();
+
+					System.out.println("Digite o Saldo da Conta (R$): ");
+					saldo = scan.nextFloat();
+
+					tipo = contaController.retornaTipo(numero);
+
+					switch (tipo) {
+					case 1 -> {
+						System.out.println("Digite o Limite de Crédito (R$): ");
+						limite = scan.nextFloat();
+						contaController.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+					}
+					case 2 -> {
+						System.out.println("Digite o dia do Aniversario da Conta: ");
+						aniversario = scan.nextInt();
+						contaController
+								.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+					}
+					default -> {
+						System.out.println("Tipo de conta inválido!");
+					}
+					}
+
+				} else
+					System.out.println("\nConta não encontrada!");
+
 				keyPress();
 				break;
+
 			case 5:
 				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n");
+
+				System.out.println("Digite o número da conta: ");
+				numero = scan.nextInt();
+
+				contaController.deletar(numero);
+
 				keyPress();
 				break;
+
 			case 6:
 				System.out.println(Cores.TEXT_WHITE + "Saque\n");
 				keyPress();
 				break;
+
 			case 7:
 				System.out.println(Cores.TEXT_WHITE + "Depósito\n");
 				keyPress();
 				break;
+
 			case 8:
 				System.out.println("Transferência entre Contas\n");
 				keyPress();
 				break;
+
 			case 9:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Banco Javeiro - O seu Futuro começa aqui!\n");
 				System.exit(0);
 				keyPress();
 				break;
+
 			default:
 				System.err.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
 				keyPress();
